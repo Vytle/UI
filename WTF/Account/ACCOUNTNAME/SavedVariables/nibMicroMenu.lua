9b6d1766a1f7e9ed3ffdeb9feadbb90d9973056a
Vytle <v1nk@vytle.com>
2013-07-31 13:38:32 +0100

    code cleanup

--- WTF/Account/ACCOUNTNAME/SavedVariables/nibMicroMenu.lua
+++ WTF/Account/ACCOUNTNAME/SavedVariables/nibMicroMenu.lua
@@ -1,20 +1,6 @@
-
 nibMicroMenuDB = {
 	["profileKeys"] = {
-		["Vessie - Jaedenar"] = "Default",
-		["Scoil - Jaedenar"] = "Default",
-		["Vonkk - Jaedenar"] = "Default",
-		["Xenz - Jaedenar"] = "Default",
-		["Klimi - Jaedenar"] = "Default",
-		["Zawk - Jaedenar"] = "Default",
-		["Kuli - Jaedenar"] = "Default",
-		["Frux - Jaedenar"] = "Default",
 		["Vink - Jaedenar"] = "Default",
-		["Vinkeh - Grim Batol"] = "Default",
-		["Kutski - Grim Batol"] = "Default",
-		["Mizt - Jaedenar"] = "Default",
-		["Vonok - Jaedenar"] = "Default",
-		["Skoil - Grim Batol"] = "Default",
 	},
 	["profiles"] = {
 		["Default"] = {
